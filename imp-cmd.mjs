2cf50a7d2180c1e2faee0649cd5c0c6bd8da3b63
tangentstorm <michal.wallace@gmail.com>
2024-02-13 14:14:14 -0500

    imp-cmd: new unadorned repl for tanco

--- imp-cmd.mjs
+++ imp-cmd.mjs
@@ -1,5 +1,24 @@
 #!/usr/bin/node
+import { T } from "./imp-core.mjs";
 import { ImpReader } from "./imp-read.mjs";
-import * as fs from "fs";
-var stdinBuffer = fs.readFileSync(0); // STDIN_FILENO = 0
-console.log(stdinBuffer.toString());
+import { ImpWriter } from "./imp-write.mjs";
+import { ImpEvaluator } from "./imp-eval.mjs";
+import * as readline from "readline";
+
+let impR = new ImpReader();
+let impW = new ImpWriter();
+let impE = new ImpEvaluator();
+
+let rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false });
+
+async function repl() {
+  for await (const line of rl) {
+    impR.send(line)
+    let r = impR.read()
+    let e = impE.eval(r)
+    if (e[0] !== T.NIL) console.log(impW.show(e)) }}
+
+repl()
