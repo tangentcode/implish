7650059aacd8c44546e0f4606ead642343083c11
tangentstorm <michal.wallace@gmail.com>
2024-02-13 18:07:08 -0500

    introduce parts of speech and add T.JDY

--- imp-core.mjs
+++ imp-core.mjs
@@ -3,15 +3,32 @@
 export function ok() { }  // the empty program
 
 export let T = {  // types
-  TOP: 'TOP',     // top-level sequence (list with no delimiters)
   SEP: 'SEP',     // separator
+  // --- values with literal representation
+  TOP: 'TOP',     // top-level sequence (list with no delimiters)
   INT: 'INT',     // integer
   STR: 'STR',     // string
   MLS: 'MLS',     // multi-line string
   SYM: 'SYM',     // symbol
   LST: 'LST',     // list
+  // ---- internal / refined types (require eval() to produce)
   NIL: 'NIL',     // empty/unit value
+  JSF: 'JSF',     // javascript function
+  JDY: 'JDY',     // javascript dyad (infix operator)
 };
+
+export let P = {  // parts of speech
+  V: 'V',     // verb
+  N: 'N',     // noun (data)
+  A: 'A',     // adverb
+  C: 'C',     // conjunction (infix between verbs)
+  G: 'G',     // getter / gerund (like a :get-word in red)
+  P: 'P',     // preposition (takes noun argument)
+  Q: 'Q',     // quote - `symbol
+  S: 'S',     // setter / like :set-word in red
+  M: 'M',     // method / adjective (symbol starting with ".")
+  O: 'O',     // operator (infix between nouns)
+}
 
 export let nil = [T.NIL, {}, null];  // the empty value
 
