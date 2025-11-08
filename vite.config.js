1a530aba7ce9c44c1499f4af6c7229c8c305ffbe
tangentstorm <michal.wallace@gmail.com>
2025-11-08 20:16:12 +0000

    add vite to build the online repl

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,27 @@
 import { defineConfig } from 'vite'
 import { hostname } from 'os'
 import { resolve } from 'path'
+import { execSync } from 'node:child_process'
+
+function ensureCoreBuild() {
+  let built = false
+
+  const runBuild = () => {
+    if (built) return
+    execSync('npm run build:core', { stdio: 'inherit' })
+    built = true
+  }
+
+  return {
+    name: 'implish-core-build',
+    buildStart() {
+      runBuild()
+    },
+    configureServer() {
+      runBuild()
+    }
+  }
+}
 
 export default defineConfig({
   root: './web',
@@ -8,9 +29,13 @@ export default defineConfig({
   build: {
     outDir: '../dist-web'
   },
+  plugins: [
+    ensureCoreBuild()
+  ],
   resolve: {
     alias: {
-      '/dist': resolve(__dirname, './dist')
+      '/dist': resolve(__dirname, './dist'),
+      '@misc': resolve(__dirname, './misc')
     }
   },
   server: {
